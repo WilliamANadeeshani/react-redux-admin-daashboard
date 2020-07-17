b2b0fc8f30ff191fb976fe509d1c0fdbb7ab6adf
Nadeeshani William <nadeeshanijayarathna93@gmail.com>
2020-07-17 10:33:57 +0530

    Updated .gitignore

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+import AdminDashboard from './app/adminDashboard'
+import { Provider } from 'react-redux'
+import store from './store/store'
+
+
+const app = (
+    <Provider store={store}>
+        <AdminDashboard />
+    </Provider>
+);
+render(app, document.getElementById('root'));
